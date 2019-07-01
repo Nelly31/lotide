5ac49c77269a6a92fb0284af8964a3f71b24b929
Nelly31 <nelly_main@hotmail.com>
2019-07-01 13:30:12 +0000

    connecting to the assertEqual function

---
+++ head.js
@@ -0,0 +1,9 @@
+//connecting the assertEqual function
+const assertEqual = require('./assertEqual');
+
+// function to get the first item in an array
+const head = function(array) {
+  return array[0];
+};
+
+module.exports = head;
