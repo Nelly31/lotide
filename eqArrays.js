e751de3028b69c86628b122056fb617a8448116e
Nelly31 <nelly_main@hotmail.com>
2019-07-01 13:59:23 +0000

    exporting the function

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+
+let eqArrays = function(firstArray,secondArray) {
+  let truthfulness = true;
+  // looping through the first array
+  for (let i = 0; i < firstArray.length; i++) {
+  //looping second array
+    for (let j = 0; j < secondArray.length; j++) {
+      if (secondArray[i] !== firstArray[i]) {
+        truthfulness = false;
+        break;
+      }
+    }
+  } return truthfulness;
+};
+
+module.exports = eqArrays;
